3d9b06cfcc65dc6473aa41dee357520cf505c09c
Abla-AlJawamees <aabujamous2707@gmail.com>
2025-10-05 23:26:40 +0300

    update

--- src/MyTestCases.java
+++ src/MyTestCases.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -30,24 +32,56 @@ public class MyTestCases {
 
 	@Test(priority = 2)
 	public void CheckTheDefaultCurrencyIsSAR() {
-		String ActualCurrency = driver.findElement(By.cssSelector(".__ds__comp.MuiBox-root.alm-desktop-1282bi2")).getText();
+		String ActualCurrency = driver.findElement(By.cssSelector(".__ds__comp.MuiBox-root.alm-desktop-1282bi2"))
+				.getText();
 		String ExpectedCurrency = "SAR";
 		Assert.assertEquals(ActualCurrency, ExpectedCurrency);
 	}
 
 	@Test(priority = 3)
 	public void CheckContactNumber() {
-		String ActualNumber = driver.findElement(By.cssSelector(".__ds__comp.undefined.MuiBox-root.alm-desktop-h0bow9")).getText();
+		String ActualNumber = driver.findElement(By.cssSelector(".__ds__comp.undefined.MuiBox-root.alm-desktop-h0bow9"))
+				.getText();
 		String ExpectedNumber = "+966554400000";
 		Assert.assertEquals(ActualNumber, ExpectedNumber);
 	}
-	@Test (priority = 4)
-	public void  ChechQitafLogoIsThereInTheFooter() {
-	boolean ActualResult =	driver.findElement(By.cssSelector(".alm-desktop-1mqexmc-acceptedCards")).isDisplayed();
-	boolean ExpectedResult = true;
-	Assert.assertEquals(ActualResult, ExpectedResult);
+
+	@Test(priority = 4)
+	public void CheckQitafLogoIsThereInTheFooter() {
+		boolean ActualResult = driver.findElement(By.cssSelector(".alm-desktop-1mqexmc-acceptedCards")).isDisplayed();
+		boolean ExpectedResult = true;
+		Assert.assertEquals(ActualResult, ExpectedResult);
 
 	}
-	
+
+	@Test(priority = 5)
+	public void CheckHotelTabIsNotSelected() {
+		WebElement HotelTab = driver.findElement(By.id("tab-hotels"));
+		String ActualValue = HotelTab.getAttribute("aria-selected");
+		String ExpectedValue = "false";
+		Assert.assertEquals(ActualValue, ExpectedValue);
+	}
+
+	@Test(priority = 6)
+	public void CheckDepartureDate() {
+		int today = LocalDate.now().getDayOfMonth();
+		int Tomorrow = LocalDate.now().plusDays(1).getDayOfMonth();
+
+		String ActualDeparture = driver.findElement(By.cssSelector("#testIdPickerPrefix__DatePicker__DepartureDate"))
+				.getAttribute("value");
+		Assert.assertTrue(ActualDeparture.contains(Integer.toString(Tomorrow)),
+				"Departure date does not match expected tomorrow's date");
+	}
+
+	@Test(priority = 7)
+	public void CheckReturnDate() {
+		int today = LocalDate.now().getDayOfMonth();
+		int AfterTomorrow = LocalDate.now().plusDays(2).getDayOfMonth();
+
+		String ActualCheckReturnDate = driver
+				.findElement(By.cssSelector("#testIdPickerPrefix__DatePicker__ArrivalDate")).getAttribute("value");
+		Assert.assertTrue(ActualCheckReturnDate.contains(Integer.toString(AfterTomorrow)),
+				"Return date does not match expected tomorrow's date");
+	}
 
 }
